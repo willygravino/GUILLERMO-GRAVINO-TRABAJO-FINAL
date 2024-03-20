a437634ebe3d0d0330a7ffac4d536f96eb842a99
Guillermo Gravino <ggravino@gmail.com>
2024-03-20 14:14:18 -0300

    probando gunicorn

--- AdminVideos/models.py
+++ AdminVideos/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+    #https://www.geeksforgeeks.org/urlfield-django-models/#field-options
+    #models.URLField(max_length=200, **options)
+
+#from django.db import models
+#from django.db.models import Model
+# Create your models here.
+
+#class GeeksModel(Model):
+   # geeks_field = models.URLField(max_length = 200)
+
+
 class Video(models.Model):
     nombre_video = models.CharField(max_length=30)
     url_video = models.CharField(max_length=80, blank=False)
@@ -9,29 +20,26 @@ class Video(models.Model):
     propietario = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="propietario")
     image = models.ImageField("Subí una imagen que identifique al video (o un fotograma del mismo)", upload_to="videos/", null=True, blank=True)
     fecha_video= models.DateTimeField("Fecha de captura del video:")
-
     @property
     def image_url(self):
         return self.image.url if self.image else '/media/videos/nuestrotubo.png'
-
     def __str__(self):
         return f"{self.id} - {self.nombre_video}"
-    
+     
     
 class Profile(models.Model):
      user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
      nombre_completo = models.CharField(max_length=80, blank=False)
      avatar = models.ImageField(upload_to="avatares", null=True, blank=True)
-
      @property
      def avatar_url(self):
         return self.avatar.url if self.avatar else '/media/avatares/default-avatar.png'
-     
-     
+
+
+
+
 class Mensaje(models.Model):
     mensaje = models.TextField(max_length=1000)
     email = models.EmailField()
     creado_el = models.DateTimeField(auto_now_add=True) 
     destinatario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="mensajes")
-
-
